blinksh/blink

Fix accented chars and fix positioning marked text. Fixes #462

Blink/TermInput.m
@@ -221,10 +221,6 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
   NSMutableDictionary *_functionTriggerKeys;
   NSString *_specialFKeysRow;
   NSSet<NSString *> *_imeLangSet;
-  
-  // option + e on iOS lets introduce an accented character, that we override
-  BOOL _disableAccents;
-  BOOL _dismissInput;
 
   NSMutableArray<UIKeyCommand *> *_kbdCommands;
   NSMutableArray<UIKeyCommand *> *_kbdCommandsWithoutAutoRepeat;
@@ -394,65 +390,8 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
   _skipTextStorageDelete = NO;
 }
 
-- (void)textViewDidChange:(UITextView *)textView
-{
-  if (textView.text.length == 0) {
-    _markedText = nil;
-    _skipTextStorageDelete = YES;
-    [self reset];
-    _skipTextStorageDelete = NO;
-    return;
-  }
-  
-  if (!self.markedTextRange) {
-    if (_markedText) {
-      [self _insertText:_markedText];
-      [self reset];
-      _markedText = nil;
-      [self.termDelegate.termView setIme: @"" completionHandler:nil];
-      return;
-    }
-    
-    _skipTextStorageDelete = NO;
-    _markedText = nil;
-    
-    return;
-  }
-
-  NSString *str = [self textInRange:self.markedTextRange];
-  _markedText = str;
-  
-  [self.termDelegate.termView setIme: str
-                   completionHandler:^(id data, NSError * _Nullable error) {
-    if (!data) {
-      return;
-    }
-    
-    CGRect rect = CGRectFromString(data[@"markedRect"]);
-
-    CGFloat suggestionsHeight = 44;
-    CGFloat maxY = CGRectGetMaxY(rect);
-    CGFloat minY = CGRectGetMinY(rect);
-    if (maxY - suggestionsHeight < 0) {
-      rect.origin.y = maxY;
-    } else {
-      rect.origin.y = minY - suggestionsHeight;
-    }
-    rect.size.height = 0;
-    self.frame = rect;
-  }];
-}
-
 - (void)_insertText:(NSString *)text
 {
-  if (_disableAccents) {
-    // If the accent switch is on, the next character should remove them.
-    //CFStringTransform((__bridge CFMutableStringRef)mtext, nil, kCFStringTransformStripCombiningMarks, NO);
-    text = [[NSString alloc] initWithData:[text dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]
-                                 encoding:NSASCIIStringEncoding];
-    _disableAccents = NO;
-  }
-  
   // Discard CAPS on characters when caps are mapped and there is no SW keyboard.
   BOOL capsWithoutSWKeyboard = !self.softwareKB && [self _capsMapped];
   if (capsWithoutSWKeyboard && text.length == 1 && [text characterAtIndex:0] > 0x1F) {
@@ -495,12 +434,57 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
     }
   }
 }
+  
+- (void)setMarkedText:(NSString *)markedText selectedRange:(NSRange)selectedRange
+{
+  [super setMarkedText:markedText selectedRange:selectedRange];
+  _markedText = markedText;
+  [self.termDelegate.termView setIme: _markedText
+                   completionHandler:^(id data, NSError * _Nullable error) {
+     if (!data) {
+       return;
+     }
+     
+     CGRect rect = CGRectFromString(data[@"markedRect"]);
+     
+     CGFloat suggestionsHeight = 44;
+     CGFloat maxY = CGRectGetMaxY(rect);
+     CGFloat minY = CGRectGetMinY(rect);
+     if (maxY - suggestionsHeight < 0) {
+       rect.origin.y = maxY;
+     } else {
+       rect.origin.y = minY - suggestionsHeight;
+     }
+     rect.size.height = 0;
+     self.frame = rect;
+  }];
+}
+  
+- (void)unmarkText
+{
+  [super unmarkText];
+  if (_markedText) {
+    [self _insertText:_markedText];
+  }
+  [self reset];
+}
+  
+//- (void)textViewDidChange:(UITextView *)textView
+//{
+//  if (textView.text.length == 0) {
+////    _markedText = nil;
+//    _skipTextStorageDelete = YES;
+//    [self reset];
+//    _skipTextStorageDelete = NO;
+//  }
+//}
 
 - (void)insertText:(NSString *)text
 {
   [self _insertText:text];
   
   if (_markedText) {
+    [self reset];
     return;
   }
   
@@ -518,6 +502,13 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
 
 - (void)deleteBackward
 {
+  if (_markedText.length == 1) {
+    _markedText = nil;
+    _skipTextStorageDelete = YES;
+    [self unmarkText];
+    _skipTextStorageDelete = NO;
+    return;
+  }
   // Send a delete backward key to the buffer
   [_termDelegate write:@"\x7f"];
   
@@ -968,7 +959,7 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
   NSArray<UIKeyCommand *> * commands = _kbdCommands;
   NSString *lang = self.textInputMode.primaryLanguage;
   
-  if (lang && [_imeLangSet containsObject:lang]) {
+  if ((lang && [_imeLangSet containsObject:lang]) || _markedText) {
     commands = _kbdCommandsWithoutAutoRepeat;
   }
 

ewernn/blink-fork

Blink/TermInput.m
@@ -222,10 +222,6 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
   NSMutableDictionary *_functionTriggerKeys;
   NSString *_specialFKeysRow;
   NSSet<NSString *> *_imeLangSet;
-  
-  // option + e on iOS lets introduce an accented character, that we override
-  BOOL _disableAccents;
-  BOOL _dismissInput;
 
   NSMutableArray<UIKeyCommand *> *_kbdCommands;
   NSMutableArray<UIKeyCommand *> *_kbdCommandsWithoutAutoRepeat;
@@ -396,65 +392,8 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
   _skipTextStorageDelete = NO;
 }
 
-- (void)textViewDidChange:(UITextView *)textView
-{
-  if (textView.text.length == 0) {
-    _markedText = nil;
-    _skipTextStorageDelete = YES;
-    [self reset];
-    _skipTextStorageDelete = NO;
-    return;
-  }
-  
-  if (!self.markedTextRange) {
-    if (_markedText) {
-      [self _insertText:_markedText];
-      [self reset];
-      _markedText = nil;
-      [_device.view setIme: @"" completionHandler:nil];
-      return;
-    }
-    
-    _skipTextStorageDelete = NO;
-    _markedText = nil;
-    
-    return;
-  }
-
-  NSString *str = [self textInRange:self.markedTextRange];
-  _markedText = str;
-  
-  [_device.view setIme: str
-       completionHandler:^(id data, NSError * _Nullable error) {
-    if (!data) {
-      return;
-    }
-    
-    CGRect rect = CGRectFromString(data[@"markedRect"]);
-
-    CGFloat suggestionsHeight = 44;
-    CGFloat maxY = CGRectGetMaxY(rect);
-    CGFloat minY = CGRectGetMinY(rect);
-    if (maxY - suggestionsHeight < 0) {
-      rect.origin.y = maxY;
-    } else {
-      rect.origin.y = minY - suggestionsHeight;
-    }
-    rect.size.height = 0;
-    self.frame = rect;
-  }];
-}
-
 - (void)_insertText:(NSString *)text
 {
-  if (_disableAccents) {
-    // If the accent switch is on, the next character should remove them.
-    //CFStringTransform((__bridge CFMutableStringRef)mtext, nil, kCFStringTransformStripCombiningMarks, NO);
-    text = [[NSString alloc] initWithData:[text dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]
-                                 encoding:NSASCIIStringEncoding];
-    _disableAccents = NO;
-  }
-  
   // Discard CAPS on characters when caps are mapped and there is no SW keyboard.
   BOOL capsWithoutSWKeyboard = !self.softwareKB && [self _capsMapped];
   if (capsWithoutSWKeyboard && text.length == 1 && [text characterAtIndex:0] > 0x1F) {
@@ -499,12 +438,57 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
     }
   }
 }
+  
+- (void)setMarkedText:(NSString *)markedText selectedRange:(NSRange)selectedRange
+{
+  [super setMarkedText:markedText selectedRange:selectedRange];
+  _markedText = markedText;
+  [self.device.view setIme: _markedText
+         completionHandler:^(id data, NSError * _Nullable error) {
+     if (!data) {
+       return;
+     }
+     
+     CGRect rect = CGRectFromString(data[@"markedRect"]);
+     
+     CGFloat suggestionsHeight = 44;
+     CGFloat maxY = CGRectGetMaxY(rect);
+     CGFloat minY = CGRectGetMinY(rect);
+     if (maxY - suggestionsHeight < 0) {
+       rect.origin.y = maxY;
+     } else {
+       rect.origin.y = minY - suggestionsHeight;
+     }
+     rect.size.height = 0;
+     self.frame = rect;
+  }];
+}
+  
+- (void)unmarkText
+{
+  [super unmarkText];
+  if (_markedText) {
+    [self _insertText:_markedText];
+  }
+  [self reset];
+}
+  
+//- (void)textViewDidChange:(UITextView *)textView
+//{
+//  if (textView.text.length == 0) {
+////    _markedText = nil;
+//    _skipTextStorageDelete = YES;
+//    [self reset];
+//    _skipTextStorageDelete = NO;
+//  }
+//}
 
 - (void)insertText:(NSString *)text
 {
   [self _insertText:text];
   
   if (_markedText) {
+    [self reset];
     return;
   }
   
@@ -522,6 +506,13 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
 
 - (void)deleteBackward
 {
+  if (_markedText.length == 1) {
+    _markedText = nil;
+    _skipTextStorageDelete = YES;
+    [self unmarkText];
+    _skipTextStorageDelete = NO;
+    return;
+  }
   // Send a delete backward key to the buffer
   [_device write:@"\x7f"];
   
@@ -1000,7 +991,7 @@ NSString *const TermViewAutoRepeateSeq = @"autoRepeatSeq:";
   NSArray<UIKeyCommand *> * commands = _kbdCommands;
   NSString *lang = self.textInputMode.primaryLanguage;
   
-  if (lang && [_imeLangSet containsObject:lang]) {
+  if ((lang && [_imeLangSet containsObject:lang]) || _markedText) {
     commands = _kbdCommandsWithoutAutoRepeat;
   }
 